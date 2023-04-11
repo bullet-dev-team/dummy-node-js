07a2761e0e09828c47f986453f18e23b0729ad6c
bullet-dev-team <72478731+bullet-dev-team@users.noreply.github.com>
2023-04-11 14:02:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send("<h1> Hello world </h1>");
+app.get('/env', (req, res) => {
+  res.send(process.env);
 });
 
 app.listen(port, () => {
